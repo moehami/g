d3fb8caea1fc13a56a8e29df8bc77b71d3aff837
moehami <m.aljaaly@yahoo.co.uk>
2024-09-02 17:32:13 +0300

    Update google_maps_get_results.py

--- code examples/google_maps_get_results.py
+++ code examples/google_maps_get_results.py
@@ -4,7 +4,7 @@ import requests
 response = requests.request(
     'GET',
     'http://data.oxylabs.io/v1/queries/{job_id}/results',
-    auth=('USERNAME', 'PASSWORD')
+    auth=('moehamid', 'Enterne@24')
 )
 
 # This will return the JSON response with results.
